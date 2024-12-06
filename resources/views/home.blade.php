c7e6dc0ef2842aa2cd040f645394fa72ca948143
ShayanRezayi <shayanrezayi14@gmail.com>
2024-12-06 05:03:50 +0330

    update hom blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,57 +1,60 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fa">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>فروشگاه آنلاین</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/styles.css') }}">
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
-    <h1>Home</h1>
-    <p>Welcome to my home page</p>
 
-    @if (session('message'))
-        <div>{{ session('message') }}</div>
-    @endif
+    <div class="nav">
+        {{-- <h1>Home</h1>
+        <p>Welcome to my home page</p>
 
-    @if (session('error'))
-        <div>{{ session('error') }}</div>
-    @endif
+        @if (session('message'))
+            <div>{{ session('message') }}</div>
+        @endif
 
-    <li>
-        <a href="{{ route('about') }}">About</a>
-    </li>
-    <li>
-        <a href="{{ route('contact') }}">Contact</a>
-    </li>
+        @if (session('error'))
+            <div>{{ session('error') }}</div>
+        @endif
 
-    <li>
-        <a href="{{ route('admindashboard') }}">admindashboard</a>
-    </li>
-
-    {{-- لینک‌های میهمان --}}
-    @guest
         <li>
-            <a href="{{ route('login') }}">Login</a>
+            <a href="{{ route('about') }}">About</a>
         </li>
         <li>
-            <a href="{{ route('singup') }}">Sign Up</a>
+            <a href="{{ route('contact') }}">Contact</a>
         </li>
-    @endguest
 
-    {{-- لینک‌های کاربر واردشده --}}
-    @auth
         <li>
-            <a href="{{ route('dashboard') }}">Panel</a>
-        </li>
-        <li>
-            <form action="{{ route('logout') }}" method="POST">
-                @csrf
-                <button type="submit">Logout</button>
-            </form>
-        </li>
+            <a href="{{ route('admindashboard') }}">admindashboard</a>
+        </li> --}}
+
+        {{-- لینک‌های میهمان --}}
+        {{-- @guest
+            <li>
+                <a href="{{ route('login') }}">Login</a>
+            </li>
+            <li>
+                <a href="{{ route('singup') }}">Sign Up</a>
+            </li>
+        @endguest --}}
+
+        {{-- لینک‌های کاربر واردشده --}}
+        {{-- @auth
+            <li>
+                <a href="{{ route('dashboard') }}">Panel</a>
+            </li>
+            <li>
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+            </li>
     @endauth
 
 </body>
